fc178e84e52178f77ceb2715ee0b449e70d649d1
Garceller <92178583+GarcelCode@users.noreply.github.com>
2022-02-19 14:59:00 -0600

    First Commit

---
+++ src/Views/Login.tsx
@@ -0,0 +1,9 @@
+export function Login() {
+    return (
+        <div>
+            <form>
+                <h1 className="text-blue-400">Log In</h1>
+            </form>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Login } from './Views/Login';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Login />,
   document.getElementById('root')
 );
 
